9d25d8bcbef4b82d4a6bf2b40f02b7ca968e2270
Aashish Chauhan <ashishchauhnnn@gmail.com>
2024-08-16 11:56:28 +0530

    progress

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import arrow from "../../assets/arrow_icon.png"
+import logo from '../../assets/logo1.png'
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <img src={logo} alt="" srcset="" />
+            <ul>
+                <li>Home</li>
+                <li>Features</li>
+                <li>Price</li>
+                <li>Blog</li>
+            </ul>
+            <div className='nav-right'>
+                <select>
+                    <option value="usd">USD</option>
+                    <option value="inr">INR</option>
+                    <option value="eur">EUR</option>
+                </select>
+                <button>Sign Up <img src={arrow} alt="" srcset="" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
